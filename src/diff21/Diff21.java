a77a5ec126b852ebb0abfa072e4a2fe3468952aa
Lauren Holloway <laurenholloway1010@gmail.com>
2020-01-04 13:16:59 -0500

    init repo

---
+++ src/diff21/Diff21.java
@@ -0,0 +1,17 @@
+package diff21;
+
+public class Diff21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int value = diff21(2);
+		
+	}
+	
+	public static int diff21(int n) {
+		  
+	}
+
+
+}
